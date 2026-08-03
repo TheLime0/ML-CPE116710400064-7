40a773c40ba43a4a3aa4cca16a176e1da6848e25
TheLime0 <153822888+TheLime0@users.noreply.github.com>
2026-08-03 10:26:40 +0700

    Update and rename ph to data_loader.py

---
+++ ML-04-kNN/lab1/classification/data_loader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+data = dogs_dataset.cv
+X = data.data
+y = data.target
+
